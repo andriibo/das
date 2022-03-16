6de1c917567aded1a2656a91f5e2d15ad8558375
random <andrey.bondarenko@techs.com.ua>
2022-03-16 20:40:46 +0200

    feat(DAS-61): Goalserve

---
+++ app/Clients/GoalserveClient.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Clients;
+
+use App\Exceptions\GoalserveClientException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+class GoalserveClient
+{
+    private string $apiUrl = 'https://www.goalserve.com';
+    private Client $client;
+    private ?string $apiKey;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->apiKey = config('goalserve.api_key');
+    }
+
+    public function getCricketTeams(int $leagueId): array
+    {
+        $endpoint = "{$this->apiUrl}/getfeed/{$this->apiKey}/cricketfixtures/intl/{$leagueId}_squads?json=1";
+
+        try {
+            $response = $this->client->get($endpoint);
+            $data = json_decode($response->getBody()->getContents(), true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new GoalserveClientException('Can\'t parse json - ' . json_last_error_msg());
+            }
+
+            return $data['squads']['category']['team'] ?? [];
+        } catch (ClientException $clientException) {
+            throw new GoalserveClientException($clientException->getMessage(), $clientException->getCode());
+        }
+    }
+}
