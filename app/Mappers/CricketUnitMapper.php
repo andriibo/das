69251138deec2bc20f66aa775f9d05c86bec578a
Yurii <sklema.yurii@gmail.com>
2022-09-07 13:44:49 +0300

    fix(FS-523): prepare cricket unit position

--- app/Mappers/CricketUnitMapper.php
+++ app/Mappers/CricketUnitMapper.php
@@ -3,17 +3,21 @@
 namespace App\Mappers;
 
 use App\Dto\CricketUnitDto;
-use App\Enums\CricketUnit\PositionEnum;
+use App\Services\Cricket\GetCricketPositionEnumService;
 
 class CricketUnitMapper
 {
+    public function __construct(private readonly GetCricketPositionEnumService $getCricketPositionEnumService)
+    {
+    }
+
     public function map(array $data): CricketUnitDto
     {
         $cricketTeamPlayerDto = new CricketUnitDto();
 
         $cricketTeamPlayerDto->teamId = $data['team_id'];
         $cricketTeamPlayerDto->playerId = $data['player_id'];
-        $cricketTeamPlayerDto->position = PositionEnum::tryFrom(ucwords($data['position']));
+        $cricketTeamPlayerDto->position = $this->getCricketPositionEnumService->handle($data['position']);
         $cricketTeamPlayerDto->fantasyPoints = $data['fantasy_points'] ?? null;
         $cricketTeamPlayerDto->fantasyPointsPerGame = $data['fantasy_points_per_game'] ?? null;
 
